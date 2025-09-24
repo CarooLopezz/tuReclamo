3820cf978dc1308176760212a6d7026b149f121d
iariis <iarabecerra24@gmail.com>
2025-09-24 00:41:50 -0300

    .

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
+import os
 from flask import Flask
 from backend.models.db import db
 from backend.routes.director_sector import director_sector
-from backend.routes.UserTyperoutes import user_type_bp  # NUEVO
+from dotenv import load_dotenv
 
-
-
+load_dotenv()  # carga las variables del .env
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tu_base_de_datos.db'
+
+# Configuración usando las variables del .env
+db_user = os.getenv("MYSQL_USER")
+db_password = os.getenv("MYSQL_PASSWORD")
+db_host = os.getenv("MYSQL_HOST")
+db_name = os.getenv("MYSQL_DB")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
- #Registrar rutas
+# Registrar rutas
 app.register_blueprint(director_sector)
 app.register_blueprint(user_type_bp)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
 
