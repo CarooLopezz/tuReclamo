2a2400cd8d1197b917f8679a2ed366bef52274e8
iariis <iarabecerra24@gmail.com>
2025-09-24 00:46:59 -0300

    Configura app.py para usar MySQL y agrega blueprint user_type_bp

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
+import os
 from flask import Flask
 from backend.models.db import db
 from backend.routes.director_sector import director_sector
-from backend.routes.UserTyperoutes import user_type_bp  # NUEVO
+from backend.routes.UserTyperoutes import user_type_bp
+from dotenv import load_dotenv
 
-
-
+# Cargar variables del .env
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tu_base_de_datos.db'
 
+# Configurar SQLAlchemy con MySQL
+db_user = os.getenv("MYSQL_USER")
+db_password = os.getenv("MYSQL_PASSWORD")
+db_host = os.getenv("MYSQL_HOST")
+db_name = os.getenv("MYSQL_DB")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Inicializar DB
 db.init_app(app)
 
- #Registrar rutas
+# Registrar rutas
 app.register_blueprint(director_sector)
 app.register_blueprint(user_type_bp)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
-
